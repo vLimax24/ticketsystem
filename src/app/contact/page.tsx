73db54d17a5f8a3424c0b68d39b969ce54dc7fd9
Linas <limax.workspace@gmail.com>
2024-01-13 12:52:41 +0100

    updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useState } from 'react'
-import { NextPage } from 'next'; // Import NextPage type
 
 import { Button } from "@/components/ui/button"
 import {
@@ -15,54 +14,42 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 
-interface ContactProps {} // Define an empty interface for the props
-
-const Contact: NextPage<ContactProps> = () => { // Use NextPage<ContactProps> for the component
-    const [projectId, setProjectId] = useState("")
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [description, setDescription] = useState("")
+export function page() {
 
     const handleSubmit = async (e:any) => {
         e.preventDefault();
-    
-        setProjectId("");
-        setName("");
-        setDescription("");
-        setEmail("");
-    };
-
-    return (
-        <div className='flex justify-center items-center mt-20'>
+      };
+  return (
+    <div className='flex justify-center items-center mt-20'>
             <Card className="w-[500px]">
-                <CardHeader>
-                    <CardTitle>Want to get in touch with us?</CardTitle>
-                    <CardDescription>Hit me up with a message!</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="client">Name</Label>
-                                <Input id="client" placeholder="John Doe" onChange={(e) => setProjectId(e.target.value)} value={projectId}/>
-                            </div>
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="name" placeholder="johndoe@gmail.com" onChange={(e) => setEmail(e.target.value)} value={email}/>
-                            </div>
-                            <div>
-                                <Label htmlFor="description">Message</Label>
-                                <Textarea id="description" placeholder="I want to start a big project with you!" onChange={(e) => setDescription(e.target.value)} value={description}/>
-                            </div>
-                        </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <Button className="w-full" type='submit' onClick={handleSubmit}>Submit</Button>
-                </CardFooter>
-            </Card>
-        </div>
-    )
+      <CardHeader>
+        <CardTitle>Want to get in touch with us?</CardTitle>
+        <CardDescription>Hit me up with a message!</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="client">Name</Label>
+              <Input id="client" placeholder="John Doe"/>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input id="name" placeholder="johndoe@gmail.com"/>
+            </div>
+            <div>
+                <Label htmlFor="description">Message</Label>
+                <Textarea id="description" placeholder="I want to start a big project with you!"/>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button className="w-full" type='submit' onClick={handleSubmit}>Submit</Button>
+      </CardFooter>
+    </Card>
+    </div>
+  )
 }
 
-export default Contact
+export default page
