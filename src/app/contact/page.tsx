b1b83d40201fa6dde7ad14add7bf1c4460207e5c
Linas <limax.workspace@gmail.com>
2024-01-13 13:08:11 +0100

    updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
-'use client'
-import { useState } from 'react'
-
-import { Button } from "@/components/ui/button"
+'use strict';
+import { useState } from 'react';
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -9,47 +8,47 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
-export function page() {
+export function Page() {
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = async (e:any) => {
-        e.preventDefault();
-      };
   return (
     <div className='flex justify-center items-center mt-20'>
-            <Card className="w-[500px]">
-      <CardHeader>
-        <CardTitle>Want to get in touch with us?</CardTitle>
-        <CardDescription>Hit me up with a message!</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-          <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="client">Name</Label>
-              <Input id="client" placeholder="John Doe"/>
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input id="name" placeholder="johndoe@gmail.com"/>
-            </div>
-            <div>
+      <Card className="w-[500px]">
+        <CardHeader>
+          <CardTitle>Want to get in touch with us?</CardTitle>
+          <CardDescription>Hit me up with a message!</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="client">Name</Label>
+                <Input id="client" placeholder="John Doe" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="johndoe@gmail.com" />
+              </div>
+              <div>
                 <Label htmlFor="description">Message</Label>
-                <Textarea id="description" placeholder="I want to start a big project with you!"/>
+                <Textarea id="description" placeholder="I want to start a big project with you!" />
+              </div>
             </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button className="w-full" type='submit' onClick={handleSubmit}>Submit</Button>
-      </CardFooter>
-    </Card>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button className="w-full" type='submit' onClick={handleSubmit}>Submit</Button>
+        </CardFooter>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
