27fbf650956012631d3f94abb0f5a119e078beb6
Limax <limax.workspace@gmail.com>
2024-01-18 21:27:10 +0100

    Update DashboardFetch.tsx

--- src/components/pageComponents/dashboard/DashboardFetch.tsx
+++ src/components/pageComponents/dashboard/DashboardFetch.tsx
@@ -21,15 +21,16 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer"
 import { Input } from "@/components/ui/input"
-
+import Link from 'next/link'
+import { Badge } from "@/components/ui/badge"
 interface Report {
   _id: string;
   projectId: string;
   name: string;
   email: string;
   description: string;
-  status: string;
   relevance: string;
+  status: string;
   date: string;
   __v: number;
 }
@@ -75,12 +76,6 @@ const DashboardFetch: React.FC = () => {
     }
   };
 
-  const toggleDescription = (reportId: string) => {
-    setExpandedDescription((prevDescription) => (
-      prevDescription === reportId ? null : reportId
-    ));
-  };
-
   const reassignTask = async (reportId: string) => {
     try {
       const response = await fetch(`/api/tasks/update/${reportId}`, {
@@ -115,9 +110,44 @@ const DashboardFetch: React.FC = () => {
     setId(reportId)
   }
 
+  const capitalizeFirstLetter = (text:any) => {
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+  };
+
+  const getTailwindClasses = (relevance:any) => {
+    switch (relevance) {
+      case 'critical':
+        return 'bg-red-600';
+      case 'high':
+        return 'bg-red-500';
+      case 'major':
+        return 'bg-red-400';
+      case 'medium':
+        return 'bg-yellow-500';
+      case 'low':
+        return 'bg-yellow-600';
+      case 'minor':
+        return 'bg-green-600';
+      case 'trivial':
+        return 'bg-green-500';
+      case 'PENDING':
+        return 'bg-blue-800';
+      case 'PROGRESS':
+        return 'bg-blue-700';
+      case 'HOLD':
+        return 'bg-blue-600';
+      case 'CANCELLED':
+        return 'bg-blue-500';
+      case 'COMPLETED':
+        return 'bg-blue-400';
+      default:
+        return '';
+    }
+  };
+
+
   return (
     <div >
-      <Drawer>
       <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
         {reports.map(report => (
           <Card className="sm:w-[300px] md:w-[400px] lg:w-[450px] mx-10 my-10 overflow-hidden" key={report?._id}>
@@ -127,62 +157,32 @@ const DashboardFetch: React.FC = () => {
             <CardContent>
               <div className="grid w-full items-center gap-4">
               <div>
-                  <Label htmlFor="status">Status</Label>
-                  <CardDescription>{report?.status}</CardDescription>
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Description</Label>
-                  <CardDescription className={expandedDescription === report._id ? '' : 'line-clamp-1 cursor-pointer'} onClick={() => toggleDescription(report._id)}>{report?.description}</CardDescription>
-                </div>
+                <Label htmlFor="name">Relevance</Label>
+                <CardDescription>
+                  <Badge className={`mt-2 ${getTailwindClasses(report?.relevance)} hover:cursor-pointer text-white`}>
+                    {report?.relevance ? capitalizeFirstLetter(report.relevance) : ''}
+                  </Badge>
+                </CardDescription>
+              </div>
                 <div>
-                  <Label htmlFor="name">Relevance</Label>
-                  <CardDescription>{report?.relevance}</CardDescription>
-                </div>
-                <div>
-                  <Label htmlFor="name">Project ID</Label>
-                  <CardDescription>{report?.projectId}</CardDescription>
+                  <Label htmlFor="name">Status</Label>
+                  <CardDescription>
+                  <Badge className={`mt-2 ${getTailwindClasses(report?.status)} hover:cursor-pointer text-white`}>
+                    {report?.status ? capitalizeFirstLetter(report.status) : ''}
+                  </Badge>
+                  </CardDescription>
                 </div>
               </div>
             </CardContent>
             <CardFooter className="flex justify-between w-full flex-col">
-              
-                <Button className='w-full' onClick={() => markAsDone(report._id)}>
-                  Mark as done
-                </Button>
-                <DrawerTrigger asChild>
-                  <Button className='w-full mt-3' variant={'outline'} onClick={() => idModifyer(report?._id)}>Re-assign Task</Button>
-                </DrawerTrigger>
+                <Link href={`/dashboard/${report?._id}`} className='w-full'>
+                  <Button className='w-full mt-3' onClick={() => idModifyer(report?._id)}>See More</Button>
+                </Link>
             </CardFooter>
-            <DrawerContent>
-        <div className="mx-auto w-full max-w-sm">
-          <DrawerHeader>
-            <DrawerTitle>Re-assign</DrawerTitle>
-            <DrawerDescription>Enter the new Email to re-asign the task</DrawerDescription>
-          </DrawerHeader>
-          <div className="p-4 pb-0">
-            <div className="flex flex-col space-y-1.5 my-5">
-              <Label htmlFor="email">Email of the Recipiant</Label>
-              <Input id="name" placeholder="Enter the Email of your recipiant" onChange={(e) => setNewEmail(e.target.value)} value={newEmail}/>
-            </div>
-          </div>
-          <DrawerFooter>
-            <DrawerClose asChild>
-            <Button onClick={() => reassignTask(report?._id)}>Submit</Button>
-            </DrawerClose>
-            <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </div>
-      </DrawerContent>
           </Card>
           
         ))}
       </ul>
-
-
-
-    </Drawer>
     </div>
   );
 };
