2fcf2f20a6b8be2d919f4a25c14a5a1deda057a8
Linas <limax.workspace@gmail.com>
2024-01-27 22:30:34 +0100

    finished

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,13 +2,22 @@
 
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 const AuthenticationPage = () => {
     return (
         <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
+            <div className="max-w-xl w-full space-y-8">
                 <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+                <h1 className="text-2xl md:text-[2rem] lg:text-[3rem] font-bold h-fit mb-5">Sign in to your account</h1>
                 </div>
                 <div className="space-y-6">
                     <button
@@ -22,12 +31,6 @@ const AuthenticationPage = () => {
                         className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm bg-white text-base font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                     >
                         Sign in with Google
-                    </button>
-                    <button
-                        onClick={() => signIn('email')}
-                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Sign in with Email
                     </button>
                 </div>
             </div>
