44616f657522ded403aef8da1065dd881fbc46a1
Linas <limax.workspace@gmail.com>
2024-01-27 12:11:51 +0100

    upd

--- src/app/api/outbox/route.ts
+++ src/app/api/outbox/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from 'next/server'
 import connect from '../../../app/lib/db'
 import Report from '../../../app/models/issues'
 
-export const POST = async (request) => {
+export const POST = async (request:any) => {
     try {
         await connect()
         const { senderEmail } = await request.json()
         const products = await Report.find({ senderEmail })
         return new NextResponse(JSON.stringify(products), { status: 200})
     } catch (error) {
-        return new NextResponse("Error in searching Products", error, { status: 500})
+        return new NextResponse("Error in searching Products" + error, { status: 500})
     }
 }
